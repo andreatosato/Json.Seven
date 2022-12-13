16fa8a8e0406f0b1f3280b03b5c70667dc68312f
Andrea Tosato <andrea.tosato@hudi.it>
2022-12-13 16:33:26 +0100

    Add Client

---
+++ src/Grpc.ClientApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using Grpc.App;
+using Grpc.Net.Client;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7273");
+var client = new Greeter.GreeterClient(channel);
+var response = await client.SayHelloAsync(new HelloRequest() { Name = "From Client" });
+Console.WriteLine(response);
